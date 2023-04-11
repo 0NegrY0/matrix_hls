98bc9cd21dcb864195c3549fbfbcc55d49f8e21a
NegrY <48329860+0NegrY0@users.noreply.github.com>
2023-04-10 22:53:43 -0300

    Update matrixmul_test.cpp
    testar no vivado

--- matrixmul_test.cpp
+++ matrixmul_test.cpp
@@ -1,48 +1,3 @@
-/*******************************************************************************
-'	Vendor: Xilinx
-Associated Filename: matrixmul_test.cpp
-Purpose: Vivado HLS tutorial example 
-Device: All 
-Revision History: March 1, 2013 - initial release
-                                                
-*******************************************************************************
-Copyright 2008 - 2013 Xilinx, Inc. All rights reserved.
-
-This file contains confidential and proprietary information of Xilinx, Inc. and 
-is protected under U.S. and international copyright and other intellectual 
-property laws.
-
-DISCLAIMER
-This disclaimer is not a license and does not grant any rights to the materials 
-distributed herewith. Except as otherwise provided in a valid license issued to 
-you by Xilinx, and to the maximum extent permitted by applicable law: 
-(1) THESE MATERIALS ARE MADE AVAILABLE "AS IS" AND WITH ALL FAULTS, AND XILINX 
-HEREBY DISCLAIMS ALL WARRANTIES AND CONDITIONS, EXPRESS, IMPLIED, OR STATUTORY, 
-INCLUDING BUT NOT LIMITED TO WARRANTIES OF MERCHANTABILITY, NON-INFRINGEMENT, OR 
-FITNESS FOR ANY PARTICULAR PURPOSE; and (2) Xilinx shall not be liable (whether 
-in contract or tort, including negligence, or under any other theory of 
-liability) for any loss or damage of any kind or nature related to, arising under 
-or in connection with these materials, including for any direct, or any indirect, 
-special, incidental, or consequential loss or damage (including loss of data, 
-profits, goodwill, or any type of loss or damage suffered as a result of any 
-action brought by a third party) even if such damage or loss was reasonably 
-foreseeable or Xilinx had been advised of the possibility of the same.
-
-CRITICAL APPLICATIONS
-Xilinx products are not designed or intended to be fail-safe, or for use in any 
-application requiring fail-safe performance, such as life-support or safety 
-devices or systems, Class III medical devices, nuclear facilities, applications 
-related to the deployment of airbags, or any other applications that could lead 
-to death, personal injury, or severe property or environmental damage 
-(individually and collectively, "Critical Applications"). Customer asresultes the 
-sole risk and liability of any use of Xilinx products in Critical Applications, 
-subject only to applicable laws and regulations governing limitations on product 
-liability. 
-
-THIS COPYRIGHT NOTICE AND DISCLAIMER MUST BE RETAINED AS PART OF THIS FILE AT 
-ALL TIMES.
-
-*******************************************************************************/
 #include <iostream>
 #include "matrixmul.h"
 
@@ -50,48 +5,77 @@ using namespace std;
 
 int main(int argc, char **argv)
 {
-   mat_a_t in_mat_a[6][6] = {
-      {85,85,85,85,85,85},
-      {85,85,85,85,85,85},
-      {85,85,85,85,85,85},
-      {85,85,85,85,85,85},
-      {85,85,85,85,85,85},
-      {85,85,85,85,85,85}
+   mat_a_t in_mat_a[2][2] = {
+		  {2,5},
+		  {3,7}
    };
-   mat_b_t in_mat_b[6][6] = {
-	   {85,85,85,85,85,85},
-	   {85,85,85,85,85,85},
-	   {85,85,85,85,85,85},
-	   {85,85,85,85,85,85},
-	   {85,85,85,85,85,85},
-	   {85,85,85,85,85,85}
+   mat_b_t in_mat_b[2][2] = {
+		   {1,2},
+		   {4,6}
+   };
+   mat_c_t in_mat_c[2][2] = {
+		   {8,4},
+		   {1,2}
+   };
+   mat_d_t in_mat_d[2][2] = {
+		   {9,4},
+		   {5,7}
    };
 
-   mat_b_t mat_error[6][6] = {
-	   {0,0,0,0,0,0},
-	   {0,0,0,0,0,0},
-	   {0,0,0,0,0,0},
-	   {0,0,0,0,0,0},
-	   {0,0,0,0,0,0},
-	   {0,0,0,0,0,0}
-  };
+   mat_a_t mat_error[2][2] = {
+		   {0,0},
+		   {0,0}
+   };
 
 
-   result_t hw_result[6][6], sw_result[6][6];
+   result_t hw_result[2][2];
+   result_t sw_result[2][2], mux1_result[2][2], mux2_result[2][2], add_result[2][2];
    int err_cnt = 0;
 
    // Generate the expected result
+
    // Iterate over the rows of the A matrix
    for(int i = 0; i < MAT_A_ROWS; i++) {
       for(int j = 0; j < MAT_B_COLS; j++) {
          // Iterate over the columns of the B matrix
-         sw_result[i][j] = 0;
+         mux1_result[i][j] = 0;
          // Do the inner product of a row of A and col of B
          for(int k = 0; k < MAT_B_ROWS; k++) {
-            sw_result[i][j] += in_mat_a[i][k] * in_mat_b[k][j];
+            mux1_result[i][j] += in_mat_a[i][k] * in_mat_b[k][j];
          }
       }
    }
+
+   // Iterate over the rows of the C matrix
+     for(int i = 0; i < MAT_C_ROWS; i++) {
+        for(int j = 0; j < MAT_D_COLS; j++) {
+           // Iterate over the columns of the D matrix
+           mux2_result[i][j] = 0;
+           // Do the inner product of a row of C and col of D
+           for(int k = 0; k < MAT_D_ROWS; k++) {
+              mux2_result[i][j] += in_mat_c[i][k] * in_mat_d[k][j];
+           }
+        }
+     }
+
+   // ADD
+     for(int i = 0; i < MAT_A_ROWS; i++){
+    	 for(int j = 0; j < MAT_D_COLS; j++){
+    		 add_result[i][j] = mux1_result[i][j] + mux2_result[i][j];
+    	 }
+     }
+
+   //TEST
+     for(int i = 0; i < MAT_A_ROWS; i++){
+    	 for(int j = 0; j < MAT_D_COLS; j++){
+    		if(add_result[i][j] >= 1050){
+    			sw_result[i][j] = 0;
+    		}
+    		else{
+    			sw_result[i][j] = add_result[i][j];
+    		}
+    	 }
+     }
 
    // Compare TB vs HW C-model and/or RTL
 
@@ -99,7 +83,8 @@ int main(int argc, char **argv)
 
 
    // Run matrix multiply block
-   matrixmul(in_mat_a,in_mat_b, hw_result);
+   matrixmul(in_mat_a, in_mat_b, in_mat_c, in_mat_d, hw_result);
+
    //matrixmul(in_mat_a,mat_error, hw_result); //simulate error
 
 
